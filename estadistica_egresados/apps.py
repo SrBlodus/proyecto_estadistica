81402dec101d7090a6892eaefc5c81443b7cbe04
aabr <aabenitezrivero@gmail.com>
2025-05-17 10:07:30 -0300

    Se ha configurado la inserción automática inicial de los datos normalizados

--- estadistica_egresados/apps.py
+++ estadistica_egresados/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EstadisticaEgresadosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'estadistica_egresados'
+
+    def ready(self):
+        import estadistica_egresados.signals  # Cargar señales
